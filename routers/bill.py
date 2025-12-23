12e599f73572f39bf0448974cb3148e325b501ac
mosbahmessaoud <mosbah7messaoud@gmail.com>
2025-12-23 19:55:50 +0100

    update the post new bill router , i have add the automatic calcuate the credit based on the ClientAccount v93

--- routers/bill.py
+++ routers/bill.py
@@ -498,8 +498,8 @@ def create_bill(
         ).all()
     ) + new_bill.total_remaining)
 
-    # if client_account.total_remaining == 0:
-    #     client_account.total_amount -= total_amount
+    if client_account.total_remaining == 0:
+        client_account.total_amount = 0
 
     if unpaid_bills:
         client_account.total_amount = sum(
